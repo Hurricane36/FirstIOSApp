6b4185d035d0921720a90c24a97f86510aea5fb1
Hurricane36 <43070976+Hurricane36@users.noreply.github.com>
2018-10-09 08:44:21 -0600

    Added a File

---
+++ FirstIOSApp/Model/ColorTool.swift
@@ -0,0 +1,12 @@
+//
+//  ColorTool.swift
+//  FirstIOSApp
+//
+//  Created by Tolman, Caiden on 10/9/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class ColorTool
